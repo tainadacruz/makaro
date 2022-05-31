bd9ac8f8031f7ad7a8fe179919a509db708e93c7
Raquel Behrens <raquel.behrens@hotmail.com>
2022-05-31 18:30:35 -0300

    correção na matriz do makaro

--- matrix.hs
+++ matrix.hs
@@ -87,7 +87,7 @@ makaro =
     [[(1,1,0,Arrow 1),         (1,2,2,Possible [1..4]),  (1,3,2,Fixed 4),          (1,4,0,Arrow 3),          (1,5,3,Possible [1..3]),  (1,6,4,Possible [1..4]),  (1,7,4,Fixed 4),          (1,8,4,Fixed 1)],
     [(2,1,1,Possible [1..2]),  (2,2,2,Possible [1..4]),  (2,3,2,Possible [1..4]),  (2,4,3,Possible [1..3]),  (2,5,3,Fixed 1),          (2,6,4,Fixed 2),          (2,7,6,Fixed 1),          (2,8,0,Arrow 2)],
     [(3,1,1,Possible [1..2]),  (3,2,0,Arrow 1),          (3,3,5,Fixed 4),          (3,4,5,Fixed 2),          (3,5,0,Black),            (3,6,6,Possible [1..5]),  (3,7,6,Fixed 2),          (3,8,6,Fixed 4)],
-    [(4,1,4,Fixed 2),          (4,2,4,Possible [1..2]),  (4,3,0,Arrow 4),          (4,4,5,Possible [1..4]),  (4,5,5,Fixed 3),          (4,6,9,Possible [1..3]),  (4,7,0,Arrow 1),          (4,8,6,Fixed 5)],
+    [(4,1,16,Fixed 2),          (4,2,16,Possible [1..2]),  (4,3,0,Arrow 4),          (4,4,5,Possible [1..4]),  (4,5,5,Fixed 3),          (4,6,9,Possible [1..3]),  (4,7,0,Arrow 1),          (4,8,6,Fixed 5)],
     [(5,1,0,Arrow 1),          (5,2,7,Possible [1..3]),  (5,3,7,Possible [1..3]),  (5,4,8,Possible [1..4]),  (5,5,8,Possible [1..4]),  (5,6,9,Possible [1..3]),  (5,7,9,Possible [1..3]),  (5,8,10,Possible [1..4])],
     [(6,1,4,Possible [1]),     (6,2,14,Possible [1..5]), (6,3,7,Possible [1..3]),  (6,4,0,Arrow 4),          (6,5,8,Fixed 2),          (6,6,8,Fixed 1),          (6,7,0,Arrow 2),          (6,8,10,Fixed 2)],
     [(7,1,0,Arrow 1),          (7,2,14,Fixed 5),         (7,3,13,Possible [1..3]), (7,4,13,Fixed 2),         (7,5,12,Possible [1..2]), (7,6,0,Arrow 1),          (7,7,10,Fixed 3),         (7,8,10,Possible [1..4])],
@@ -146,7 +146,7 @@ filtrar :: (Int -> [Int] -> Bool) -> [Int] -> [Int] -> [Int]
 filtrar funcao lista fixedValues = [a | a <- lista, funcao a fixedValues]
 
 getCell :: Cell -> Int -> [Int] -> Cell
-getCell (a, b, c, d) regiao fixedValues | (regiao == c && (isPossible (getValue (a,b,c,d)))) = (a, b, c, Possible (filtrar ehIgual (getPossibleValue d) fixedValues))
+getCell (a, b, c, d) regiao fixedValues | (regiao == c && (isPossible (d))) = (a, b, c, Possible (filtrar ehIgual (getPossibleValue d) fixedValues))
                                         | otherwise = (a, b, c, d)
 
 getiLine :: Row -> Int -> [Int] -> Row
@@ -430,6 +430,9 @@ main = do
     print(sudoku_pruned) -}
 
     let quantityOfRegions = amountOfRegions makaro
+
+    let teste = getFixedValuesOfMatrix makaro 4
+    print(teste)
 
     let makaro_pruned = pruningCellPossibilities makaro quantityOfRegions
 
