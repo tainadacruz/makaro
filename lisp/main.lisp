aaadaa7fe85f478967038d3519d53acb17cae4af
Vitor Hugo <vitormarzarotto@hotmail.com>
2022-06-27 17:59:26 -0300

    Criação da estrutura item

--- lisp/main.lisp
+++ lisp/main.lisp
@@ -1,12 +1,33 @@
 (defun matriz()
-    "Matriz
-    ----------------
-    | Makaro Solver |
-    ---------------- "
+    '(
+        '((create NIL NIL 3 ) (create NIL NIL 3 ) )
+        '((create NIL NIL 3 ) (create NIL NIL 3 ))
+    )
+)
+
+(defstruct item
+    arrow 
+    black
+    possible 
+)
+(defun create(a b possible)
+    (setq p1
+        (make-item 
+            :arrow a
+            :black b
+            :possible possible
+        )
+    )
+    p1
 )
 
 (defun main()
-    (write-string (matriz))
+    (write (matriz))
+    (setq p1
+        (create NIL NIL '(1 2 3 4 5))
+    )
+    (setf (item-possible p1) 3)
+    (write p1)
 )
 
 (main)
