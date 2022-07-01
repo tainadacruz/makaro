0b7811e773bd2b8d4ab4b47aba3a75ce0c0f965d
Raquel Behrens <raquel.behrens@hotmail.com>
2022-06-30 22:03:53 -0300

    Matriz Makaro Lisp

--- lisp/main.lisp
+++ lisp/main.lisp
@@ -7,6 +7,19 @@
     )
 )
 
+(defun makaro2()
+    '(
+        '((create T NIL 0 (1))    (create NIL NIL 2 ())   (create NIL NIL 2 (4))  (create T NIL 0 (3))    (create NIL NIL 3 ())  (create NIL NIL 4 ())   (create NIL NIL 4 (4))  (create NIL NIL 4 (1)))
+        '((create NIL NIL 1 ())   (create NIL NIL 2 ())   (create NIL NIL 2 ())   (create NIL NIL 3 ())   (create NIL NIL 3 (1)) (create NIL NIL 4 (2))  (create NIL NIL 6 (1))  (create T NIL 0 (2)))
+        '((create NIL NIL 1 ())   (create T NIL 0 (1))    (create NIL NIL 5 (4))  (create NIL NIL 5 (2))  (create NIL T 0 ())    (create NIL NIL 6 ())   (create NIL NIL 6 (2))  (create NIL NIL 6 (4)))
+        '((create NIL NIL 16 (2)) (create NIL NIL 16 ())  (create T NIL 0 (4))    (create NIL NIL 5 ())   (create NIL NIL 5 (3)) (create NIL NIL 9 ())   (create T NIL 0 (1))    (create NIL NIL 6 (5)))
+        '((create T NIL 0 (1))    (create NIL NIL 7 ())   (create NIL NIL 7 ())   (create NIL NIL 8 ())   (create NIL NIL 8 ())  (create NIL NIL 9 ())   (create NIL NIL 9 ())   (create NIL NIL 10 ()))
+        '((create NIL NIL 17 ())  (create NIL NIL 14 ())  (create NIL NIL 7 ())   (create T NIL 0 (4))    (create NIL NIL 8 (2)) (create NIL NIL 8 (1))  (create T NIL 0 (2))    (create NIL NIL 10 (2)))
+        '((create T NIL 0 (1))    (create NIL NIL 14 (5)) (create NIL NIL 13 ())  (create NIL NIL 13 (2)) (create NIL NIL 12 ()) (create T NIL 0 (1))    (create NIL NIL 10 (3)) (create NIL NIL 10 ()))
+        '((create NIL NIL 14 (3)) (create NIL NIL 14 ())  (create NIL NIL 14 (4)) (create NIL NIL 13 ())  (create NIL NIL 12 ()) (create NIL NIL 11 (1)) (create NIL NIL 11 ())  (create T NIL 0 (4)))
+    )
+)
+
 (defun main()
     (write (matriz))
     (setq p1
