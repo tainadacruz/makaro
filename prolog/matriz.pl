a1362f587ae0a8e0b2ff6f8c92b0deed78a22b3c
Vitor Hugo <vitormarzarotto@hotmail.com>
2022-07-10 11:09:04 -0300

    Inicializando repositório com matriz

---
+++ prolog/matriz.pl
@@ -0,0 +1,10 @@
+:-initialization(main).
+
+
+main:- write([
+    [cell(nil,nil,1,[1,2,4]),cell(nil,nil,1,[1,4,5])],
+    [cell(nil,nil,1,[5,6,7]),cell(nil,nil,1,[8,1,2])]
+
+]),
+write('\n'),
+halt.
