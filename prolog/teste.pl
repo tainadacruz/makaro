e9cfcf6a2a5ffad75eae8365fe18ea3906733091
Raquel Behrens <raquel.behrens@hotmail.com>
2022-07-13 18:45:55 -0300

    progresso

--- prolog/teste.pl
+++ prolog/teste.pl
@@ -23,7 +23,126 @@ solucao(TabuleiroSolucao) :-
                                  [X61, X62, X63, X64, X65, X66, X67, X68],
                                  [X71, X72, X73, X74, X75, X76, X77, X78],
                                  [X81, X82, X83, X84, X85, X86, X87, X88]].
+
                                 
+    %linha1
+    completa(X11, TabuleiroSolucao),
+    completa(X12, TabuleiroSolucao),
+    completa(X13, TabuleiroSolucao),
+    completa(X14, TabuleiroSolucao),
+    completa(X15, TabuleiroSolucao),
+    completa(X16, TabuleiroSolucao),
+    completa(X17, TabuleiroSolucao),
+    completa(X18, TabuleiroSolucao),
+
+    %linha2
+    completa(X21, TabuleiroSolucao),
+    completa(X22, TabuleiroSolucao),
+    completa(X23, TabuleiroSolucao),
+    completa(X24, TabuleiroSolucao),
+    completa(X25, TabuleiroSolucao),
+    completa(X26, TabuleiroSolucao),
+    completa(X27, TabuleiroSolucao),
+    completa(X28, TabuleiroSolucao),
+
+    %linha3
+    completa(X31, TabuleiroSolucao),
+    completa(X32, TabuleiroSolucao),
+    completa(X33, TabuleiroSolucao),
+    completa(X34, TabuleiroSolucao),
+    completa(X35, TabuleiroSolucao),
+    completa(X36, TabuleiroSolucao),
+    completa(X37, TabuleiroSolucao),
+    completa(X38, TabuleiroSolucao),
+
+    %linha4
+    completa(X41, TabuleiroSolucao),
+    completa(X42, TabuleiroSolucao),
+    completa(X43, TabuleiroSolucao),
+    completa(X44, TabuleiroSolucao),
+    completa(X45, TabuleiroSolucao),
+    completa(X46, TabuleiroSolucao),
+    completa(X47, TabuleiroSolucao),
+    completa(X48, TabuleiroSolucao),
+
+    %linha5
+    completa(X51, TabuleiroSolucao),
+    completa(X52, TabuleiroSolucao),
+    completa(X53, TabuleiroSolucao),
+    completa(X54, TabuleiroSolucao),
+    completa(X55, TabuleiroSolucao),
+    completa(X56, TabuleiroSolucao),
+    completa(X57, TabuleiroSolucao),
+    completa(X58, TabuleiroSolucao),
+
+    %linha6
+    completa(X61, TabuleiroSolucao),
+    completa(X62, TabuleiroSolucao),
+    completa(X63, TabuleiroSolucao),
+    completa(X64, TabuleiroSolucao),
+    completa(X65, TabuleiroSolucao),
+    completa(X66, TabuleiroSolucao),
+    completa(X67, TabuleiroSolucao),
+    completa(X68, TabuleiroSolucao),
+
+    %linha7
+    completa(X71, TabuleiroSolucao),
+    completa(X72, TabuleiroSolucao),
+    completa(X73, TabuleiroSolucao),
+    completa(X74, TabuleiroSolucao),
+    completa(X75, TabuleiroSolucao),
+    completa(X76, TabuleiroSolucao),
+    completa(X77, TabuleiroSolucao),
+    completa(X78, TabuleiroSolucao),
+
+    %linha8
+    completa(X81, TabuleiroSolucao),
+    completa(X82, TabuleiroSolucao),
+    completa(X83, TabuleiroSolucao),
+    completa(X84, TabuleiroSolucao),
+    completa(X85, TabuleiroSolucao),
+    completa(X86, TabuleiroSolucao),
+    completa(X87, TabuleiroSolucao),
+    completa(X88, TabuleiroSolucao),
+
+
+
+n(1).
+n(2).
+n(3).
+n(4).
+
+
+acharElementosRegiao(_,[],[]).
+acharElementosRegiao(regiao, tabuleiro, resultado) :-
+    acharElementosRegiaoLinha(region, tabuleiro, resultado).
+
+acharElementosRegiaoLinha(regiao, [L|Ls], E) :-
+    acharElementosRegiaoColuna(regiao, L, RSs),
+    acharElementosRegiaoLinha(regiao, Ls, E1),
+    append(RSs, E1, E).
+
+acharElementosRegiaoColuna(regiao, [E|Es], E2) :-
+    acharElementosRegiaoCelula(regiao, E, E3),
+    acharElementosRegiaoColuna(regiao, Es, RSs),
+    append(E3, RSs, E2).
+
+acharElementosRegiaoCelula(regiao, cell(regiao, elemento), elemento).
+acharElementosRegiaoCelula(regiao, cell(_, elemento), _).
+
+
+
+completa(cell(0,_), _).
+completa(cell(region,value), matrix) :-
+    n(X1), n(X2), n(X3), n(X4),
+    todosDiferentes([X1, X2, X3, X4]).
+
+todosDiferentes([]).
+todosDiferentes([H|T]) :- not(member(H,T)), todosDiferentes(T).
+
+
+/*
+
     %linhas
     completa([X11, X12, X13, X14]),
     completa([X21, X22, X23, X24]),
@@ -55,6 +174,8 @@ completa([X1, X2, X3, X4]) :-
 
 todosDiferentes([]).
 todosDiferentes([H|T]) :- not(member(H,T)), todosDiferentes(T).
+
+*/      
 
 
 /*fazer função para ver os números possíveis daquela célula, baseando-se no número de células da região da célula*/
