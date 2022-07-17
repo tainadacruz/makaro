ae71134f928f173f9902e914022ce12c32026545
Raquel Behrens <raquel.behrens@hotmail.com>
2022-07-17 13:33:21 -0300

    Alguns ajustes

--- prolog/adjacency.pl
+++ prolog/adjacency.pl
@@ -93,45 +93,58 @@ verificarAdjacencias(TabuleiroSolucao) :-
     naoHaIgualAoLadoMeio(X77, X67, X76, X78, X87).
 
 
-naoHaIgualAoLadoCanto(cell(_,_,Region1, Value1), cell(_,_,Region2, Value2), cell(_,_,Region3, Value3)) :- member(Region1, [0]) ; 
+naoHaIgualAoLadoCanto(cell(_,_,Region1, Value1), cell(_,_,Region2, Value2), cell(_,_,Region3, Value3)) :- 
+    																						  member(Region1, [0]) ; 
                                                                                               (not(member(Region1, [0])) ,
-                                                                                               ((member(Region2, [0]) ;
-                                                                                                (not(member(Region2, [0])) ,
-                                                                                                 not(member(Value1, [Value2])))) ,
-                                                                                                  (member(Region3, [0]) ;
-                                                                                                   (not(member(Region3, [0])) ,
-                                                                                                    not(member(Value1, [Value3])))))).
+                                                                                                  (var(Value1) ;
+                                                                                                   ((member(Region2, [0]) ;
+                                                                                                    (not(member(Region2, [0])) ,
+                                                                                                        (var(Value2) ;
+                                                                                                         not(member(Value1, [Value2]))))) ,
+                                                                                                      (member(Region3, [0]) ;
+                                                                                                       (not(member(Region3, [0])) ,
+                                                                                                           (var(Value3) ;
+                                                                                                            not(member(Value1, [Value3])))))))).
 
 
 naoHaIgualAoLadoLinha(cell(_,_,Region1, Value1), cell(_,_,Region2, Value2), cell(_,_,Region3, Value3), cell(_,_,Region4, Value4)) :- 
                                                                                               member(Region1, [0]) ; 
                                                                                               (not(member(Region1, [0])) ,
-                                                                                               ((member(Region2, [0]) ;
-                                                                                                (not(member(Region2, [0])) ,
-                                                                                                 not(member(Value1, [Value2])))) ,
-                                                                                                  ((member(Region3, [0]) ;
-                                                                                                   (not(member(Region3, [0])) ,
-                                                                                                    not(member(Value1, [Value3])))),
-                                                                                                    (member(Region4, [0]) ;
-                                                                                                    (not(member(Region4, [0])) ,
-                                                                                                     not(member(Value1, [Value4]))))
-                                                                                                    ))).
+                                                                                                  (var(Value1) ;
+                                                                                                   ((member(Region2, [0]) ;
+                                                                                                    (not(member(Region2, [0])) ,
+                                                                                                        (var(Value2) ;
+                                                                                                         not(member(Value1, [Value2]))))) ,
+                                                                                                      ((member(Region3, [0]) ;
+                                                                                                       (not(member(Region3, [0])) ,
+                                                                                                           (var(Value3) ;
+                                                                                                            not(member(Value1, [Value3]))))),
+                                                                                                        (member(Region4, [0]) ;
+                                                                                                        (not(member(Region4, [0])) ,
+                                                                                                            (var(Value4) ;
+                                                                                                             not(member(Value1, [Value4])))))
+                                                                                                        )))).
 
 
 naoHaIgualAoLadoMeio(cell(_,_,Region1, Value1), cell(_,_,Region2, Value2), cell(_,_,Region3, Value3), cell(_,_,Region4, Value4), cell(_,_,Region5, Value5)) :- 
                                                                                               member(Region1, [0]) ; 
                                                                                               (not(member(Region1, [0])) ,
-                                                                                               ((member(Region2, [0]) ;
-                                                                                                (not(member(Region2, [0])) ,
-                                                                                                 not(member(Value1, [Value2])))) ,
-                                                                                                  ((member(Region3, [0]) ;
-                                                                                                   (not(member(Region3, [0])) ,
-                                                                                                    not(member(Value1, [Value3])))),
-                                                                                                    ((member(Region4, [0]) ;
-                                                                                                     (not(member(Region4, [0])) ,
-                                                                                                      not(member(Value1, [Value4])))),
-                                                                                                     (member(Region5, [0]) ;
-                                                                                                      (not(member(Region5, [0])) ,
-                                                                                                       not(member(Value1, [Value5])))))
-                                                                                                    ))).
+                                                                                                  (var(Value1) ,
+                                                                                                   ((member(Region2, [0]) ;
+                                                                                                    (not(member(Region2, [0])) ,
+                                                                                                        (var(Value2) ;
+                                                                                                         not(member(Value1, [Value2]))))) ,
+                                                                                                      ((member(Region3, [0]) ;
+                                                                                                       (not(member(Region3, [0])) ,
+                                                                                                           (var(Value3) ;
+                                                                                                            not(member(Value1, [Value3]))))),
+                                                                                                        ((member(Region4, [0]) ;
+                                                                                                         (not(member(Region4, [0])) ,
+                                                                                                             (var(Value4) ;
+                                                                                                              not(member(Value1, [Value4]))))),
+                                                                                                         (member(Region5, [0]) ;
+                                                                                                          (not(member(Region5, [0])) ,
+                                                                                                              (var(Value5) ;
+                                                                                                               not(member(Value1, [Value5]))))))
+                                                                                                        )))).
 
